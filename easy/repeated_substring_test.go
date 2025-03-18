5946bd1107cb0b270b2453449263b3c8227c630a
Pedro Sena <pedrolima121+github@gmail.com>
2025-03-18 16:52:42 -0300

    optimize implementation

--- easy/repeated_substring_test.go
+++ easy/repeated_substring_test.go
@@ -1,47 +1,56 @@
 package easy
 
 import (
+	"strings"
 	"testing"
 )
 
 func repeatedSubstringPattern(s string) bool {
-	var sSize int = len(s)
-	switch {
-	case sSize == 1:
-		return false
-	case sSize == 2 && s[0] != s[1]:
-		return false
-	case sSize == 2 && s[0] == s[1]:
-		return true
-	default:
-		var v string
-		for j := 2; j <= sSize; j++ {
-			sub_str := s[:j]
-			var pivot int
-			for i := j; i < sSize; i += j {
-				pivot = min(i+j, sSize)
-				next_str := s[i:pivot]
-				if sub_str != next_str {
-					break
-				}
-				sub_str = next_str
-			}
-			v = sub_str
-			if pivot == sSize {
-				break
-			}
-		}
+	var size int = len(s)
 
-		var result string
-		if sSize%len(v) > 0 {
-			result += string(v[0])
+	for i := size / 2; i > 0; i-- {
+		if size%i == 0 && strings.Repeat(s[:i], size/i) == s {
+			return true
 		}
-		for range sSize / len(v) {
-			result += v
-		}
-
-		return result == s
 	}
+	return false
+	// var sSize int = len(s)
+	// switch {
+	// case sSize == 1:
+	// 	return false
+	// case sSize == 2 && s[0] != s[1]:
+	// 	return false
+	// case sSize == 2 && s[0] == s[1]:
+	// 	return true
+	// default:
+	// 	var v string
+	// 	for j := 2; j <= sSize; j++ {
+	// 		sub_str := s[:j]
+	// 		var pivot int
+	// 		for i := j; i < sSize; i += j {
+	// 			pivot = min(i+j, sSize)
+	// 			next_str := s[i:pivot]
+	// 			if sub_str != next_str {
+	// 				break
+	// 			}
+	// 			sub_str = next_str
+	// 		}
+	// 		v = sub_str
+	// 		if pivot == sSize {
+	// 			break
+	// 		}
+	// 	}
+
+	// 	var result string
+	// 	if sSize%len(v) > 0 {
+	// 		result += string(v[0])
+	// 	}
+	// 	for range sSize / len(v) {
+	// 		result += v
+	// 	}
+
+	// 	return result == s
+	// }
 }
 
 func TestCase1(t *testing.T) {
